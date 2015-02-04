a8d79cb192f857e00fa61f3b1bd1aaa0e9c57ad9
Geronimo Orozco <geronimo.orozco@intel.com>
2015-02-04 12:45:14 -0600

    Rename answers file

--- packstack.sh
+++ packstack.sh
@@ -21,7 +21,7 @@ yum update -y
 yum install -y https://rdo.fedorapeople.org/rdo-release.rpm
 yum install -y openstack-packstack
 yum install -y epel-release erlang.x86_64
-packstack -d --answer-file=/vagrant/packstack-answers-20150201-003728.txt
+packstack -d --answer-file=/vagrant/packstack-answers.txt
 # Adding cirros image
 source /etc/environment
 source /root/keystonerc_admin
